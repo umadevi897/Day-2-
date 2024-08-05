34ce23412bb58374a854ab7e0b82eecf42f8cb38
Umadevi_veluduti <101104398+umadevi897@users.noreply.github.com>
2024-08-05 14:49:48 +0530

    Add files via upload
    Task 4, 5

---
+++ task 4 5.py
@@ -0,0 +1,44 @@
+#task 4 comment program 
+#  a=10 
+#  b=27
+#  print(a) 
+
+# task 4 variable program 
+# umadevi = 16
+#print("umadevi") 
+
+
+#a=b=c=2000
+#print(a)
+
+#a=b=c=2000
+#print(a,b,c) 
+
+# task 4 implicit and explicit data type 
+ #a=21
+ #integer--->float 
+ #print(float(a))
+ 
+ 
+ #b=161.6666
+ #print(int(b)) 
+ 
+ #task 5 simple interest 
+#p=1000
+#r=365
+#t=5 
+#si=p*r*t/1000
+#print(si) 
+# task 5 compond interst 
+
+#p=3
+#nt=12
+#r=5
+#n=20
+#t=7
+#ci=(1+r/n)*nt
+#print(ci) 
+
+
+ 
+ 
